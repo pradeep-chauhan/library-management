0531b8a89f2a5d984bec0c4cc27be53087c23eab
Pradeep Chauhan <pradeepchauhan@Pradeeps-MacBook-Pro.local>
2023-07-16 23:05:48 +0530

    added routes for books

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,7 @@ Rails.application.routes.draw do
       registrations: 'registrations'
   }
   resources :users, only: %i[index show]
+  resources :libraries, only: [:show] do
+    resources :books, only: [:index]
+  end
 end
